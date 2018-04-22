064aea698a7be4bff2a2b43cc71a1c75eccf193c
Nerita Sung <neritasung@gmail.com>
2018-04-22 18:35:24 -0500

    add psuedocode and some js

---
+++ assets/javascript/logic.js
@@ -0,0 +1,48 @@
+  // Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAXsWEOOvN9AVJaRQtTTFJ3M9lnZbekMKg",
+    authDomain: "train-scheduler-becbe.firebaseapp.com",
+    databaseURL: "https://train-scheduler-becbe.firebaseio.com",
+    projectId: "train-scheduler-becbe",
+    storageBucket: "",
+    messagingSenderId: "818126281047"
+  };
+  firebase.initializeApp(config);
+
+//  variable to reference firebase 
+var database = firebase.database();
+
+// Initial Variables
+var trainName = "";
+var destination = "";
+var trainTime = 0;
+var frequency = 0;
+
+// empty the form everytime a user click submit
+
+
+
+// on click function for the submit button (with event default)
+
+
+
+// logic to store data in database from input
+
+
+
+
+// moment.js logic to calculate the next arrival
+
+
+
+
+// moment.js logic to calculate the minutes away
+
+
+
+
+// use .on (push) the data to the table above
+
+
+
+

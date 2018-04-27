97b473686737342ead9aa2560a9d6b1af831ee51
Nerita Sung <neritasung@gmail.com>
2018-04-26 21:28:17 -0500

    adds calculation for arrival time and minutes away

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,3 +1,5 @@
+ $(document).ready(function(){
+
   // Initialize Firebase
 var config = {
     apiKey: "AIzaSyAXsWEOOvN9AVJaRQtTTFJ3M9lnZbekMKg",
@@ -20,22 +22,22 @@ var frequency = 0;
 
 // on click function for the submit button (with event default)
 $("#submit").on("click",function(){
-  console.log(abc);
     event.preventDefault();
  
 
 // logic to store data in database from input
-trainName = $("#trainName-input").val().trim();
-destination = $("#destination-input").val().trim();
-trainTime = moment($("#time-input").val().trim(), "HH:mm").format("X");
+trainName = $("#trainName-input").val();
+destination = $("#destination-input").val();
+trainTime = moment($("#time-input").val().trim(), "HH:mm").subtract(1, "years").format("X");
 frequency = $("#frequency-input").val().trim();
 
 // create object for new train input
 var newTrain = {
     trainName: trainName,
-    desination: desination,
+    destination: destination,
     trainTime: trainTime,
-    frequency: frequency
+    frequency: frequency,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
   };
 
 // use .on (push) the data to the table above
@@ -55,29 +57,62 @@ $("#destination-input").val("");
 $("#time-input").val("");
 $("#frequency-input").val("");
 
-
 });
 
-// use firebase to store data to the site using child_added and add each entry to the table
+// Looping the train_min function to capture train new time
+function train_min() {
+  database.ref().child('train-scheduler-becbe').once('value', function (snapshot) {
+      snapshot.forEach(function (child_snap) {
+          newTime = moment().format('X');
+          database.ref('train-scheduler-becbe/' + child_snap.key).update({
+              traintime: newTime,
+          })
+      })
+  });
+};
 
+//Count down the train new time every one min
+setInterval(train_min, 60000);
 
+// clear out data
+database.ref().child('train-scheduler-becbe').on('value', function (snapshot) {
+  $('tbody').empty();
+}, function (errorObject) {
+  console.log("Errors handled: " + errorObject.code);
+});
+
+// use child_added to add data from Firebase
+database.ref().orderByChild("dateAdded").on("child_added", function(childSnapshot){
 
 // store everything in a variable
 
+var snap_val = childSnapshot.val();
+var t_n = snap_val.trainName; //train name
+var t_d = snap_val.destination;//train destination
+var t_f = snap_val.frequency;//train frequency
+var t_t = snap_val.trainTime;//train start time
 
-// moment.js logic to calculate the next arrival
+// Difference between time
+var timeDiff = moment().diff(moment.unix(trainTime), "minutes");
 
+// Difference between now and train start time
+var timeRemainder = moment().diff(moment.unix(t_t), "minutes") % t_f;
+       console.log(timeRemainder);
 
-// moment.js logic to calculate the minutes away
+//  Minutes away calculation
+var min = t_f - timeRemainder;
+       console.log(min);
 
-
-
-
-
+// format next arrival time
+var nextTrainArrival = moment().add(min, "m").format("HH:mm");
 
 // use HMTL to add the data on the table
+$("#train_details").append("<tr>" + "<td>" + t_n + "</td>" + "<td>" + t_d + "</td>" +
+"<td>" + t_f + "</td>" + "<td>" + nextTrainArrival + "</td>" + "<td>" + min + "</td>" + "</tr>");
+console.log("details");
+// Handle the errors
 
-
-
-
-// code for handling errors
+   }), function (errorObject) {
+       console.log("Errors handled: " + errorObject.code);
+   }
+});
